600f1d95366c89880f423745a6e3e38d9863d2cd
kaykostadinov <kay.kostadinov@gmail.com>
2020-11-29 20:11:35 +0200

    Added double jump functionality

--- Game/src/actor/player.gd
+++ Game/src/actor/player.gd
@@ -6,16 +6,22 @@ var can_jump: bool
 var jump_pressed: bool
 var can_shoot := true
 
+export var d_jump_speed := 1000
+var just_d_jumped := false
+
 func _physics_process(delta):
 	var is_jump_interrupted := Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction := get_direction()
 	_velocity = get_move_velocity(_velocity, speed, direction, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+	
 	flip_sprite()
 	idle_anim()
 	run_anim()
 	jump_anim()
 	shoot()
+	can_double_jump()
 
 
 # return movement direction
@@ -47,14 +53,36 @@ func get_move_velocity(linear_velocity: Vector2, speed: Vector2, direction: Vect
 	ms.x = speed.x * direction.x
 	ms.y += gravity * get_physics_process_delta_time()
 	
+	
 	if direction.y == -1.0:
 		ms.y = speed.y * direction.y 
 	
 	if is_jump_interrupted:
 		ms.y = 0
 	
+	if Input.is_action_just_pressed("double_jump") and can_double_jump():
+		ms.y = -d_jump_speed
+		just_d_jumped = true
+	
 	return ms
 
+
+# double jump
+func d_jump() -> Vector2:
+	
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		-1.0 if can_double_jump() else 1.0
+		)
+
+# is double jump possible
+func can_double_jump() -> bool:
+	if is_on_floor():
+		just_d_jumped = false
+	
+	if !is_on_floor() and !is_on_wall() and !just_d_jumped:
+		return true
+	return false
 
 # timeout func to implement coyote time  
 func coyote_time():
@@ -68,12 +96,13 @@ func jump_extention():
 	jump_pressed = false
 
 
-# remember jump before landing
+# shooting interval
 func shoot_cooldown():
 	can_shoot = false
 	yield(get_tree().create_timer(.5), "timeout")
 	can_shoot = true
-	
+
+
 
 # shoot
 func shoot():
